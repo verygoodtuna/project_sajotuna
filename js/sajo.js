b256419740c52b42441fb72dd0bc2016302486ba
verygoodtuna <133613555+verygoodtuna@users.noreply.github.com>
2023-06-14 18:21:52 +0900

    Add files via upload

--- js/sajo.js
+++ js/sajo.js
@@ -1,14 +1,25 @@
 $(function () {
 
+    $('.header .banner_b .gnb').on('click', function () {
+        $('.header .lnb_con').addClass('on')
+    })
+
 
     $('.v_slide').slick ({
         arrows: false,
         dots: true,
+        slidesToShow: 1,
+        infinite: true,
+        centerMode: true,
+        centerPadding: '250px',
+
     })
+
 
     $('.main_visual .btn .prev').on('click', function () {
         $('.v_slide').slick('slickPrev')
     })
+
     $('.main_visual .btn .next').on('click', function () {
         $('.v_slide').slick('slickNext')
     })
@@ -16,5 +27,6 @@ $(function () {
     $('.p_slide').slick ({
         slidesToShow: 5,
         arrows: false,
+        infinite: true,
     })
 })
